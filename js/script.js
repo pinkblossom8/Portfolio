c171b80748a0f235639546cae06f1e20c309b72b
pinkblossom8 <samail0312@hanmail.net>
2022-12-13 16:23:37 +0900

    221213 page-4 slide 작업

--- js/script.js
+++ js/script.js
@@ -192,10 +192,29 @@ function SliderBox1__init() {
   });
 }
 
+function SliderBox2__init() {
+  const swiper = new Swiper(".slider-box-2 .swiper", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".slider-box-2 .swiper-pagination"
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".slider-box-2 .swiper-button-next",
+      prevEl: ".slider-box-2 .swiper-button-prev"
+    }
+  });
+}
+
 
 Particles__init();
 Chart__init();
 SliderBox1__init();
+SliderBox2__init();
 
 
 
