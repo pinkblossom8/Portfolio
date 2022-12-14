25e7b4fddaa446d46d2977b861f06440e2c54d71
pinkblossom8 <samail0312@hanmail.net>
2022-12-14 15:26:59 +0900

    221214 page-5 작업

--- js/script.js
+++ js/script.js
@@ -236,11 +236,11 @@ function SliderBox3__init() {
     breakpoints: {
 
       // 화면의 넓이 >= 480px
-      480: {
+      768: {
         slidesPerView: 1,
       },
       // 화면의 넓이 >= 640px
-      640: {
+      769: {
         slidesPerView: 2,
         spaceBetween: 10
       }
